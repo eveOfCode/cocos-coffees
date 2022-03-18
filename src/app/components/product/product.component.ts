35660107dea50cf138a4d618712981b3ccb0e1f9
Genevieve Twohig <eve_oftwohig@yahoo.com>
2022-03-18 14:52:39 -0300

    onPush product component

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 
 import { Product } from 'src/app/product.model';
@@ -8,13 +8,14 @@ import { selectCoffeeProductByID } from 'src/app/state/store.selectors';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductComponent implements OnInit {
 
-  @Input() coffeeProductId: number | undefined = 3;
+  // @Input() coffeeProductId: number | undefined = 3; // Not in use
 
-  ID: number = +this.route.snapshot.paramMap.get('id') as number; 
+  ID: number = +this.route.snapshot.paramMap.get('id') as number;
 
   coffeeData: Product;
   selectedCoffeeProduct$ = this.store.select(selectCoffeeProductByID(this.ID)).subscribe(val => { this.coffeeData = val; }); // used to get raw data for the table
@@ -22,7 +23,7 @@ export class ProductComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private store: Store
+    private store: Store,
   ) { }
 
   ngOnInit(): void {
